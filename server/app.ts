ba301f6de937044ddfcb5c7f341f714093765bbc
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-05-14 23:24:46 +0530

    serving the react app from fe

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,14 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
+
+app.route("/api/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist", path: "./frontend/dist/index.html" }));
 
 export default app;
