2a2726e6f485cfc55e7426cb90a2c98639f6d9cd
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-05-14 23:18:04 +0530

    fetch the expense route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
+import { useEffect } from "node_modules/@types/react/ts5.0";
 
 function App() {
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/total-spent")
+      .then((res) => res.json())
+      .then((data) => setTotalAmount(data.total));
+  }, [totalAmount]);
   return (
     <>
       <div className="flex items-center justify-center h-full w-full">
+        <h1 className="text-4xl">Total amount spent: {totalAmount}</h1>
         <Button>Click me</Button>
       </div>
     </>
